ae1ac294f91885c522ddc4e16bd194ac99a49959
Robbie <rob.w.sherman@gmail.com>
2020-12-31 14:07:07 +0000

    Add a fetch list API

--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .serializers import TaskSerializer
+from .models import *
 
 # Create your views here.
 
@@ -15,3 +19,11 @@ def apiOverview(request):
     }
 
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+
+    return Response(serializer.data)
