5997a5b9410243621504b9465a0faa7148028c0b
Robbie <rob.w.sherman@gmail.com>
2020-12-31 10:52:42 +0000

    Setup framework and baseurl

--- api/views.py
+++ api/views.py
@@ -1,9 +1,17 @@
-from django.http.response import JsonResponse
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse('API BASE POINT', safe=False)
+    api_urls = {
+        'List': '/task-list/',
+        'Create': '/task-create/',
+        'Detail': '/task-detail/<str:pk>',
+        'Update': '/task-update/<str:pk>',
+        'Delete': '/task-delete/<str:pk>',
+    }
+
+    return Response(api_urls)
