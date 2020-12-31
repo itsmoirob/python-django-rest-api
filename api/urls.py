0b13af0f1d930a6fc525967a20c19fc398af3461
Robbie <rob.w.sherman@gmail.com>
2020-12-31 14:14:58 +0000

    Add details url and view

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('task-detail/<str:pk>/', views.taskDetail, name='task-detail'),
     path('task-list/', views.taskList, name='task-list'),
     path('', views.apiOverview, name='api-overview'),
 ]
