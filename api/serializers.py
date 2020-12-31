b17271caf726ffd9c2457d95eae7e3dd72dfe696
Robbie <rob.w.sherman@gmail.com>
2020-12-31 14:06:40 +0000

    Add serializer for Tasks

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from django.db import models
+from rest_framework import serializers
+
+from .models import *
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
